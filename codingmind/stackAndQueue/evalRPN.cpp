aa8ca7da7af10f66607d7c1bda38aa76102a4d45
kam <zmgfunction@gmail.com>
2024-07-03 22:14:07 +0800

    evalRPN

--- codingmind/stackAndQueue/evalRPN.cpp
+++ codingmind/stackAndQueue/evalRPN.cpp
@@ -1,4 +1,4 @@
-#include"head.hpp"
+#include "head.hpp"
 
 /*
 逆波兰表达式求值
@@ -19,22 +19,68 @@
 解释: 该算式转化为常见的中缀算术表达式为：(4 + (13 / 5)) = 6
 */
 
-int evalRPN(const std::vector<std::string>& vec)
+int operation(int a, int b, std::string str)
 {
+    char op = str[0]; // switch只能处理字符类型 不能处理字符串类型
+    switch (op)
+    {
+    case '+':
+        return a + b;
+        break;
+    case '-':
+        return a - b;
+        break;
+    case '*':
+        return a * b;
+        break;
+    case '/':
+        return a / b;
+        break;
+    default:
+        break;
+    }
     return 0;
+}
+
+int evalRPN(const std::vector<std::string> &vec)
+{
+    std::stack<int> sta;
+    int i = 0;
+    int sum = 0;
+    int operation_1 = 0;
+    int operation_2 = 0;
+    while (i < vec.size())
+    {
+        if (vec[i] == "+" || vec[i] == "-" || vec[i] == "*" || vec[i] == "/")
+        {
+            operation_1 = sta.top();
+            sta.pop();
+            operation_2 = sta.top();
+            sta.pop();
+            sum = operation(operation_2, operation_1, vec[i]);
+            sta.push(sum);
+        }
+        else
+        {
+            int temp = stoi(vec[i]);
+            sta.push(temp);
+        }
+        i++;
+    }
+    return sum;
 }
 
 int main()
 {
     std::vector<std::string> vec;
-    std::string str;
-    std::getline(std::cin,str);
+    std::string str = "4 13 5 / +";
+    // std::getline(std::cin, str);
     std::istringstream iss(str);
-    while(iss >> str)
+    while (iss >> str)
     {
         vec.push_back(str);
     }
-    
     printVec(vec);
+    std::cout << "ans = " << evalRPN(vec) << std::endl;
     return 0;
 }
