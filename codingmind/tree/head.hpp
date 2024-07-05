587b8a458ef652fdb672dbc77b35d2552c39a3d5
kam <zmgfunction@gmail.com>
2024-07-05 19:40:16 +0800

    tree

---
+++ codingmind/tree/head.hpp
@@ -0,0 +1,16 @@
+#include "/root/file/codingmind/stackAndQueue/head.hpp"
+
+class TreeNode
+{
+public:
+    TreeNode(int val):val_(val)
+    {
+        
+    }
+    
+
+private: 
+    TreeNode *left;
+    TreeNode *right;
+    int val_;
+};
