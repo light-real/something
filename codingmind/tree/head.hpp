4ebf29e96c96fc7122a129cfebe711a4941ed841
kam <zmgfunction@gmail.com>
2024-07-05 22:37:14 +0800

    update

--- codingmind/tree/head.hpp
+++ codingmind/tree/head.hpp
@@ -39,3 +39,40 @@ TreeNode *TreeNodeInit()
     return head;
 }
 
+void recursive_Preorder_traversal(TreeNode *node, std::vector<int> &show)
+{
+    if (node == nullptr)
+    {
+        return;
+    }
+    show.push_back(node->val_);
+    recursive_Preorder_traversal(node->left, show);
+    recursive_Preorder_traversal(node->right, show);
+}
+
+void recursive_Inorder_traversal(TreeNode *node, std::vector<int> &show)
+{
+    if (node == nullptr)
+    {
+        return;
+    }
+    recursive_Preorder_traversal(node->left, show);
+    show.push_back(node->val_);
+    recursive_Preorder_traversal(node->right, show);
+}
+
+void recursive_Postorder_traversal(TreeNode *node, std::vector<int> &show)
+{
+    if (node == nullptr)
+    {
+        return;
+    }
+    recursive_Preorder_traversal(node->left, show);
+    recursive_Preorder_traversal(node->right, show);
+    show.push_back(node->val_);
+}
+
+void Preorder_traversal(TreeNode* head)
+{
+    
+}
---
+++ codingmind/tree/traversal.cpp
@@ -0,0 +1,13 @@
+#include "head.hpp"
+
+
+int main()
+{
+    std::vector<int> show;
+    TreeNode *head = TreeNodeInit();
+    // recursive_Preorder_traversal(head,show);  //先序遍历
+    // recursive_Inorder_traversal(head,show);   //中序遍历
+    recursive_Postorder_traversal(head, show); // 后续遍历
+    printVec(show);
+    return 0;
+}
