f85c69f8fab3254de909436c68a5960421ad834b
kam <zmgfunction@gmail.com>
2024-07-08 17:45:07 +0800

    update

---
+++ codingmind/tree/minDepth.cpp
@@ -0,0 +1,46 @@
+/*
+返回二叉树的最小深度
+*/
+
+#include "head.hpp"
+
+int minDepth(TreeNode* head)
+{
+    int Depth = 0;
+    std::queue<TreeNode*> que;
+    if(!head)
+    {
+        return Depth;
+    }
+    que.push(head);
+    while(!que.empty())
+    {
+        int size = que.size();
+        for(int i = 0;i<size;i++)
+        {
+            TreeNode* node = que.front();
+            que.pop();
+            if(node->left)
+            {
+                que.push(node->left);
+            }
+            if(node->right)
+            {
+                que.push(node->right);
+            }
+            if(!node->left && !node->right)
+            {
+                return Depth;
+            }
+        }
+        Depth++;
+    }
+    return Depth;
+}
+
+int main()
+{
+    TreeNode* head = TreeNodeInit();
+    std::cout<<"minDepth = "<<minDepth(head)<<std::endl;
+    return 0;
+}
