0d10e8d6cae6d9ee4fc752aefbd84eec75354ebb
kam <zmgfunction@gmail.com>
2024-07-15 11:15:23 +0800

    update

--- codingmind/tree/pathofTree.cpp
+++ codingmind/tree/pathofTree.cpp
@@ -4,36 +4,36 @@
 
 #include "head.hpp"
 
-std::vector<std::string> pathofTree(TreeNode* head)
+std::vector<std::string> pathofTree(TreeNode *head)
 {
-    std::stack<TreeNode*> treest; //用于存储节点
-    std::stack<std::string> pathst; //用于存储当前的路径
-    std::vector<std::string> result; //用于存储结果
-    if(head == nullptr)
+    std::stack<TreeNode *> treest;   // 用于存储节点
+    std::stack<std::string> pathst;  // 用于存储当前的路径
+    std::vector<std::string> result; // 用于存储结果
+    if (head == nullptr)
     {
         return result;
     }
     treest.push(head);
     pathst.push(std::to_string(head->val_));
-    while(!treest.empty())
+    while (!treest.empty())
     {
-        TreeNode* node = treest.top();
+        TreeNode *node = treest.top();
         treest.pop();
         std::string path = pathst.top();
         pathst.pop();
-        if(node->left == nullptr && node->right == nullptr)
+        if (node->left == nullptr && node->right == nullptr)
         {
             result.push_back(path);
         }
-        if(node->right)
+        if (node->right)
         {
             treest.push(node->right);
-            pathst.push(path+"->"+std::to_string(node->right->val_));
+            pathst.push(path + "->" + std::to_string(node->right->val_));
         }
-        if(node->left)
+        if (node->left)
         {
             treest.push(node->left);
-            pathst.push(path+"->"+std::to_string(node->left->val_));
+            pathst.push(path + "->" + std::to_string(node->left->val_));
         }
     }
     printVec(result);
@@ -42,8 +42,8 @@ std::vector<std::string> pathofTree(TreeNode* head)
 
 int main()
 {
-    std::vector<int> values = {1,2,3,4,5,6,7,8,9};
-    TreeNode* head = TreeNodeInitWithVector(values);
+    std::vector<int> values = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    TreeNode *head = TreeNodeInitWithVector(values);
     pathofTree(head);
     return 0;
 }
