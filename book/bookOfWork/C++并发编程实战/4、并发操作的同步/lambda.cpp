fb99a25ef0bcf052bbe1aa836c19ee4990dd535e
kam <zmgfunction@gmail.com>
2024-08-12 10:12:25 +0800

    book

---
+++ book/bookOfWork/C++并发编程实战/4、并发操作的同步/lambda.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int local_variable = 10;
+    int x = 19;
+    auto result = [=,&x](int &out)->int{  //(int &out)表示传出的参数 也即调用时需要传入的参数
+        x = 29;
+        out = x;
+        return x + local_variable;
+    };
+    int out = 0;
+    result(out);
+    cout<<"result = "<<out<<endl;
+    return 0;
+}
+
+//g++ -std=c++11 -o main lambda.cpp
